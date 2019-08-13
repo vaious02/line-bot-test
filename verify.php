5c8e1da6c2f743c64fce973e4f3daf3984f2a183
vaious02 <vaious02@gmail.com>
2019-08-13 12:52:17 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php 
   $access_token = '/qDWtmrbjLtQoXiBUL5edNsyDiOIont0wclSJZ2z9vdEaTuelky5+1RKIsEGggU9hwxxD4Nio4we0qOHAgXXe+qm+FqlI1qAPs4vJzX18LDwFGgymzP43aGMnJT64O+t/9ctf/7F0vmwA5jggaTwTgdB04t89/1O/w1cDnyilFU=';
-  $url = 'https://api.line.me/v2/oauth/verify';
+  $url = 'https://api.line.me/v2.1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
