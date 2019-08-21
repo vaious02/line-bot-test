4e28f5b6a5a39c49f9f7829460d0bc6f18865a41
vaious02 <vaious02@gmail.com>
2019-08-21 14:13:06 +0700

    bot_check_stock commit 2

--- bot_check_stock.php
+++ bot_check_stock.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://www.stylhunt.com/Sellboard24/module/line/api/api_get_detailproduct_with_name.php?p_name=" + encodeURIComponent(product_name);
+$url = "http://www.stylhunt.com/Sellboard24/module/line/api/api_get_detailproduct_with_name.php?p_name=Couple%20bow";
 $json = file_get_contents($url);
 $obj = json_decode($json);
 echo $obj->access_token;
