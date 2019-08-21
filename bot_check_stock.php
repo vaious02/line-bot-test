0403c1601c2ac01d8663f3f5d8b2c88ed1aa6a1d
vaious02 <vaious02@gmail.com>
2019-08-21 15:17:23 +0700

    bot_check_stock commit 8

--- bot_check_stock.php
+++ bot_check_stock.php
@@ -11,109 +11,103 @@ $arrayHeader[] = "Content-Type: application/json";
 $arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
 //รับข้อความจากผู้ใช้
-$message = encodeURIComponent($arrayJson['events'][0]['message']['text']);
+$message = $arrayJson['events'][0]['message']['text'];
 
 $url = "http://www.stylhunt.com/Sellboard24/module/line/api/api_get_detailproduct_with_name.php?p_name=" . $message;
 $obj = file_get_contents($url);
 // $obj = json_decode($json);
 
 $arrayPostData = array();
-$arrayPostDataCard = array();
 $arrayPostDataDetail = array();
 $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 
+
 if(count($obj) === 1) {
-  for ($i=0; $i < 1 ; $i++) {
-    for ($j=0; $j < count($obj); $j++) {
-      $arrayPostDataDetail['type'] = "box";
-      $arrayPostDataDetail['layout'] = "baseline";
-      $arrayPostDataDetail['contents'][0]['type'] = "icon";
-      $arrayPostDataDetail['contents'][0]['url'] = "https://img.icons8.com/color/48/000000/star.png";
-
-      $arrayPostDataDetail['contents'][1]['type'] = "text";
-      $arrayPostDataDetail['contents'][1]['text'] = $obj[$i]['stock'][$j]['name'];
-      $arrayPostDataDetail['contents'][1]['flex'] = "0";
-      $arrayPostDataDetail['contents'][1]['margin'] = "sm";
-      $arrayPostDataDetail['contents'][1]['weight'] = "bold";
-
-      $arrayPostDataDetail['contents'][2]['type'] = "text";
-      $arrayPostDataDetail['contents'][2]['text'] = $obj[$i]['stock'][$j]['qty'] . ' ชิ้น';
-      $arrayPostDataDetail['contents'][1]['size'] = "sm";
-      $arrayPostDataDetail['contents'][1]['align'] = "end";
-      $arrayPostDataDetail['contents'][1]['color'] = "#AAAAAA";
-    }
-    $arrayPostDataCard['type'] = "bubble";
-    $arrayPostDataCard['body']['type'] = "box";
-    $arrayPostDataCard['body']['layout'] = "vertical";
-    $arrayPostDataCard['body']['spacing'] = "md";
-    $arrayPostDataCard['body']['action']['type'] = "uri";
-    $arrayPostDataCard['body']['action']['label'] = "Action";
-    $arrayPostDataCard['body']['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $obj[$i]['product_name'];
-    $arrayPostDataCard['body']['contents'][0]['type'] = "text";
-    $arrayPostDataCard['body']['contents'][0]['text'] = $obj[$i]['product_name'];
-    $arrayPostDataCard['body']['contents'][0]['size'] = "lg";
-    $arrayPostDataCard['body']['contents'][0]['weight'] = "bold";
-    $arrayPostDataCard['body']['contents'][1]['type'] = "separator";
-    $arrayPostDataCard['body']['contents'][2]['type'] = "box";
-    $arrayPostDataCard['body']['contents'][2]['layout'] = "vertical";
-    $arrayPostDataCard['body']['contents'][2]['spacing'] = "sm";
-    $arrayPostDataCard['body']['contents'][2]['contents'] = $arrayPostDataDetail;
-    $arrayPostDataCard['body']['footer']['type'] = "box";
-    $arrayPostDataCard['body']['footer']['layout'] = "vertical";
-    $arrayPostDataCard['body']['footer']['contents'][0]['type'] = "button";
-    $arrayPostDataCard['body']['footer']['contents'][0]['action']['type'] = "uri";
-    $arrayPostDataCard['body']['footer']['contents'][0]['action']['label'] = "ดูรายละเอียด";
-    $arrayPostDataCard['body']['footer']['contents'][0]['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $obj[$i]['product_name'];
-    $arrayPostDataCard['body']['footer']['contents'][0]['color'] = "#8D769D";
-    $arrayPostDataCard['body']['footer']['contents'][0]['style'] = "primary";
-
-  }
-
   $arrayPostData['messages'][0]['type'] = "flex";
   $arrayPostData['messages'][0]['altText'] = "ส่งสตอคสินค้า";
   $arrayPostData['messages'][0]['contents']['type'] = "carousel";
-  $arrayPostData['messages'][0]['contents']['contents'] = $arrayPostDataCard;
+  for ($i=0; $i < 1 ; $i++) {
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['type'] = "bubble";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['type'] = "box";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['layout'] = "vertical";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['spacing'] = "md";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['action']['type'] = "uri";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['action']['label'] = "Action";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $obj[$i]['product_name'];
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][0]['text'] = $obj[$i]['product_name'];
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][0]['size'] = "lg";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][0]['weight'] = "bold";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][1]['type'] = "separator";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['type'] = "box";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['layout'] = "vertical";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['spacing'] = "sm";
+    for ($j=0; $j < count($obj); $j++) {
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['type'] = "box";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['layout'] = "baseline";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][0]['type'] = "icon";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][0]['url'] = "https://img.icons8.com/color/48/000000/star.png";
+
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['type'] = "text";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['text'] = $obj[$i]['stock'][$j]['name'];
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['flex'] = "0";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['margin'] = "sm";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['weight'] = "bold";
+
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][2]['type'] = "text";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][2]['text'] = $obj[$i]['stock'][$j]['qty'] . ' ชิ้น';
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['size'] = "sm";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['align'] = "end";
+      $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['contents'][2]['contents'][$j]['contents'][1]['color'] = "#AAAAAA";
+    }
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['type'] = "box";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['layout'] = "vertical";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['type'] = "button";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['action']['type'] = "uri";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['action']['label'] = "ดูรายละเอียด";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $obj[$i]['product_name'];
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['color'] = "#8D769D";
+    $arrayPostData['messages'][0]['contents']['contents'][$i]['body']['footer']['contents'][0]['style'] = "primary";
+  }
 
   replyMsg($arrayHeader,$arrayPostData);
 
 }
 
 if(count($obj) > 1) {
-  $arrayPostDataCard['type'] = "bubble";
-  $arrayPostDataCard['direction'] = "ltr";
-  $arrayPostDataCard['body']['type'] = "box";
-  $arrayPostDataCard['body']['layout'] = "vertical";
-  $arrayPostDataCard['body']['flex'] = "0";
-  $arrayPostDataCard['body']['contents'][0]['type'] = "text";
-  $arrayPostDataCard['body']['contents'][0]['text'] = $message;
-  $arrayPostDataCard['body']['contents'][0]['size'] = "lg";
-  $arrayPostDataCard['body']['contents'][0]['weight'] = "bold";
-  $arrayPostDataCard['body']['contents'][0]['wrap'] = true;
-  $arrayPostDataCard['body']['contents'][1]['type'] = "separator";
-  $arrayPostDataCard['body']['contents'][2]['type'] = "box";
-  $arrayPostDataCard['body']['contents'][3]['layout'] = "baseline";
-  $arrayPostDataCard['body']['contents'][3]['spacing'] = "sm";
-  $arrayPostDataCard['body']['contents'][3]['contents'][0]['type'] = "text";
-  $arrayPostDataCard['body']['contents'][3]['contents'][0]['text'] = "สินค้าที่ค้นหามีมากกว่า 1 ชิ้น";
-  $arrayPostDataCard['body']['contents'][3]['contents'][0]['flex'] = 0;
-  $arrayPostDataCard['body']['contents'][3]['contents'][0]['margin'] = "sm";
-  $arrayPostDataCard['body']['contents'][3]['contents'][0]['weight'] = "regular";
-  $arrayPostDataCard['footer']['type'] = "box";
-  $arrayPostDataCard['footer']['layout'] = "vertical";
-  $arrayPostDataCard['footer']['contents'][0]['type'] = "button";
-  $arrayPostDataCard['footer']['contents'][0]['action']['type'] = "uri";
-  $arrayPostDataCard['footer']['contents'][0]['action']['label'] = "ดูทั้งหมด";
-  $arrayPostDataCard['footer']['contents'][0]['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $message;
-  $arrayPostDataCard['footer']['contents'][0]['color'] = "#8D769D";
-  $arrayPostDataCard['footer']['contents'][0]['style'] = "primary";
-
-
   $arrayPostData['messages'][0]['type'] = "flex";
   $arrayPostData['messages'][0]['altText'] = "ส่งสตอคสินค้า";
   $arrayPostData['messages'][0]['contents']['type'] = "carousel";
-  $arrayPostData['messages'][0]['contents']['contents'] = $arrayPostDataCard;
+  $arrayPostData['messages'][0]['contents']['contents'][0]['type'] = "bubble";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['direction'] = "ltr";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['type'] = "box";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['layout'] = "vertical";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['flex'] = "0";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][0]['type'] = "text";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][0]['text'] = $message;
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][0]['size'] = "lg";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][0]['weight'] = "bold";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][0]['wrap'] = true;
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][1]['type'] = "separator";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][2]['type'] = "box";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['layout'] = "baseline";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['spacing'] = "sm";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['contents'][0]['type'] = "text";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['contents'][0]['text'] = "สินค้าที่ค้นหามีมากกว่า 1 ชิ้น";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['contents'][0]['flex'] = 0;
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['contents'][0]['margin'] = "sm";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['body']['contents'][3]['contents'][0]['weight'] = "regular";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['type'] = "box";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['layout'] = "vertical";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['type'] = "button";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['action']['type'] = "uri";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['action']['label'] = "ดูทั้งหมด";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['action']['uri'] = "http://www.stylhunt.com/Sellboard24/check_stock/index.html?p_name=" . $message;
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['color'] = "#8D769D";
+  $arrayPostData['messages'][0]['contents']['contents'][0]['footer']['contents'][0]['style'] = "primary";
 
-    replyMsg($arrayHeader,$arrayPostData);
+  replyMsg($arrayHeader,$arrayPostData);
+
 } else if(count($obj) === 0) {
   $arrayPostData['messages'][0]['type'] = "text";
   $arrayPostData['messages'][0]['text'] = "ไม่มีสินค้าที่คุณค้นหาค่ะ";
