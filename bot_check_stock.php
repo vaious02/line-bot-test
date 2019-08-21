64362980c3568103220fab979e52f9d9aa38953d
vaious02 <vaious02@gmail.com>
2019-08-21 14:13:56 +0700

    bot_check_stock commit 3

--- bot_check_stock.php
+++ bot_check_stock.php
@@ -3,6 +3,6 @@
 $url = "http://www.stylhunt.com/Sellboard24/module/line/api/api_get_detailproduct_with_name.php?p_name=Couple%20bow";
 $json = file_get_contents($url);
 $obj = json_decode($json);
-echo $obj->access_token;
+echo $obj;
 
 ?>
